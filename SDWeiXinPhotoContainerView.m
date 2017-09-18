2aea7d927f917ef8512f35b349cd5b967859e286
Eddie_Fan <nmgfqlong@126.com>
2017-09-18 17:15:06 +0800

    朋友圈九宫格布局计算

---
+++ SDWeiXinPhotoContainerView.m
@@ -0,0 +1,137 @@
+//
+//  SDWeiXinPhotoContainerView.m
+//  SDAutoLayout 测试 Demo
+//
+//  Created by gsd on 15/12/23.
+//  Copyright © 2015年 gsd. All rights reserved.
+//
+
+#import "SDWeiXinPhotoContainerView.h"
+
+#import "UIView+SDAutoLayout.h"
+
+#import "SDPhotoBrowser.h"
+
+@interface SDWeiXinPhotoContainerView () <SDPhotoBrowserDelegate>
+
+@property (nonatomic, strong) NSArray *imageViewsArray;
+
+@end
+
+@implementation SDWeiXinPhotoContainerView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self setup];
+    }
+    return self;
+}
+
+//先直接先创建9宫格
+- (void)setup
+{
+    NSMutableArray *temp = [NSMutableArray new];
+    
+    for (int i = 0; i < 9; i++) {
+        UIImageView *imageView = [UIImageView new];
+        [self addSubview:imageView];
+        imageView.userInteractionEnabled = YES;
+        imageView.tag = i;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapImageView:)];
+        [imageView addGestureRecognizer:tap];
+        [temp addObject:imageView];
+    }
+    
+    self.imageViewsArray = [temp copy];
+}
+
+
+- (void)setPicPathStringsArray:(NSArray *)picPathStringsArray
+{
+    _picPathStringsArray = picPathStringsArray;
+
+    for (long i = _picPathStringsArray.count; i < self.imageViewsArray.count; i++) {
+        UIImageView *imageView = [self.imageViewsArray objectAtIndex:i];
+        imageView.hidden = YES;
+    }
+    
+    if (_picPathStringsArray.count == 0) {
+        self.height = 0;
+        self.fixedHeight = @(0);
+        return;
+    }
+    
+    CGFloat itemW = [self itemWidthForPicPathArray:_picPathStringsArray];
+    CGFloat itemH = 0;
+    if (_picPathStringsArray.count == 1) {
+        UIImage *image = [UIImage imageNamed:_picPathStringsArray.firstObject];
+        if (image.size.width) {
+            itemH = image.size.height / image.size.width * itemW;
+        }
+    } else {
+        itemH = itemW;
+    }
+    long perRowItemCount = [self perRowItemCountForPicPathArray:_picPathStringsArray];
+    CGFloat margin = 5;
+    
+    //这个计算排版数据还是很巧妙的。。
+    [_picPathStringsArray enumerateObjectsUsingBlock:^(NSString *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        long columnIndex = idx % perRowItemCount;
+        long rowIndex = idx / perRowItemCount;
+        UIImageView *imageView = [_imageViewsArray objectAtIndex:idx];
+        imageView.hidden = NO;
+        imageView.image = [UIImage imageNamed:obj];
+        imageView.frame = CGRectMake(columnIndex * (itemW + margin), rowIndex * (itemH + margin), itemW, itemH);
+    }];
+    
+    CGFloat w = perRowItemCount * itemW + (perRowItemCount - 1) * margin;
+    int columnCount = ceilf(_picPathStringsArray.count * 1.0 / perRowItemCount);
+    CGFloat h = columnCount * itemH + (columnCount - 1) * margin;
+    self.width = w;
+    self.height = h;
+    
+    self.fixedHeight = @(h);
+    self.fixedWidth = @(w);
+}
+
+#pragma mark - private actions
+
+- (void)tapImageView:(UITapGestureRecognizer *)tap
+{
+    UIView *imageView = tap.view;
+    SDPhotoBrowser *browser = [[SDPhotoBrowser alloc] init];
+    browser.currentImageIndex = imageView.tag;
+    browser.sourceImagesContainerView = self;
+    browser.imageCount = self.picPathStringsArray.count;
+    browser.delegate = self;
+    [browser show];
+}
+
+//根据图片总数确定每个图片的大小
+- (CGFloat)itemWidthForPicPathArray:(NSArray *)array
+{
+    if (array.count == 1) {
+        return 170;
+    }else if ((array.count >= 2 && array.count <= 4)) {
+        return 140;
+    }else {
+        CGFloat w = [UIScreen mainScreen].bounds.size.width > 320 ? 80 : 70;
+        return w;
+    }
+}
+
+//计算九宫格图片每行的个数
+- (NSInteger)perRowItemCountForPicPathArray:(NSArray *)array
+{
+    if (array.count < 3) {
+        return array.count;
+    } else if (array.count <= 4) {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
+
+@end
